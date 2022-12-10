b8851ed5934be582fbc2239df2457930428fc140
Barış Gökmen <barisgokmen06@gmail.com>
2022-12-10 11:00:28 +0200

    Firebase Setup, google-services.json, sample temp code in main

--- app/src/main/java/com/example/queuemanagement/MainActivity.kt
+++ app/src/main/java/com/example/queuemanagement/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.queuemanagement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var db = Firebase.firestore
+
+        // TEST FOR DATABASE CONFIG - TEMP /////////////////////////
+        var textView: TextView = findViewById(R.id.textView1)
+        var mylist = StringBuilder()
+        db.collection("PersonList")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    println("${document.id} => ${document.data}")
+                    mylist.append("${document.data}")
+                }
+                textView.setText(mylist)
+            }
+            .addOnFailureListener { exception ->
+                println("Error getting documents.")
+            }
+        ////////////////////////////////////////////////////////////
+
+
+
     }
 }
