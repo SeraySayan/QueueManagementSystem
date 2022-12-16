94320da0f94c944908acb077b9677178ceac0829
eyluldenizyildiz <eylul.yildiz@metu.edu.tr>
2022-12-16 15:33:57 +0200

    branch_list_view

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -13,7 +13,6 @@ class MainActivity : AppCompatActivity() {
 
     val db = Firebase.firestore
 
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
