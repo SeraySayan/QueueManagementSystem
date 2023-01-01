5f63460cc036e411484466d7760fa7f83454817a
Barış Gökmen <barisgokmen06@gmail.com>
2023-01-01 17:43:37 +0200

    updated MainActivity.kt

--- app/src/main/java/com/example/queuemanagement/EmployeeMenu.kt
+++ app/src/main/java/com/example/queuemanagement/EmployeeMenu.kt
@@ -2,14 +2,21 @@ package com.example.queuemanagement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.queuemanagement.databinding.ActivityEmployeeMenuBinding
+import com.example.queuemanagement.databinding.ActivityMainBinding
 
 
 // TODO : binding ekle
 
 
 class EmployeeMenu : AppCompatActivity() {
+
+    lateinit var binding: ActivityEmployeeMenuBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityEmployeeMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
